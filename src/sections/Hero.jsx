e5c9c7f90d99e35bb544c9127e014613ffd566d1
Ashwani Singh <ashwanisingh3846@gmail.com>
2025-07-26 06:26:28 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -49,8 +49,8 @@ const Hero = () => {
             </div>
 
             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-              Hi, I’m Adrian, a developer based in Croatia with a passion for
-              code.
+               Hi, I’m Ashwani, a developer based in India. I love to build And
+              code..
             </p>
 
             <Button
